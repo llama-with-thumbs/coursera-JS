3b54d830b3f8ef42d995dc83aaf1ba84185e083f
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-03-06 16:39:56 -0500

    looks like it is done

--- 2.5/script.js
+++ 2.5/script.js
@@ -2,33 +2,46 @@
 
 // Код валидации формы
 (function() {
+    function numberCheck(target) {
+        var min = target.dataset.hasOwnProperty('validatorMin') ? target.dataset.validatorMin : Number.NEGATIVE_INFINITY;
+        var max = target.dataset.hasOwnProperty('validatorMax') ? target.dataset.validatorMax : Number.POSITIVE_INFINITY;
+        return isFinite(target.value) && target.value <= max && target.value >= min;
+    }
+    function lettersCheck(target) {
+        return (/^[a-zA-Zа-яА-Я]+$/.test(target.value));
+    }
+    function regexpCheck(target) {
+        var regExp = new RegExp(target.dataset.validatorPattern);
+        return regExp.test(target.value);
+    }
+    function dataValidCheck(target) {
+        if (target.dataset.hasOwnProperty('validator')) {
+            if (target.dataset.validator == 'number') {
+                return numberCheck(target);
+            } else if (target.dataset.validator == 'letters') {
+                lettersCheck(target);
+                return lettersCheck(target);
+            } else {
+                return regexpCheck(target);
+            }
+        }
+    }
+
+    function dataRequiredCheck(targetId, inputErrorClass) {
+        var target = document.getElementById(targetId);
+        console.log(target.value);
+        if ((target.dataset.hasOwnProperty('required') && !target.value)||(target.value && !dataValidCheck(target))) {
+            target.classList.add(inputErrorClass);
+        }
+    }
+
     window.validateForm = function(form) {
-        // var formKeys = Object.keys(form);
-        // formKeys.forEach(key => {
-        //     console.log(key, ':', form[key]);
-        // });
         var formId = form.formId;
         var formValidClass = form.formValidClass;
         var formInvalidClass = form.formInvalidClass;
         var inputErrorClass = form.inputErrorClass;
         var form = document.getElementById('profile');
 
-        function dataRequiredCheck(targetId, inputErrorClass) {
-            var target = document.getElementById(targetId);
-            if (target.dataset.hasOwnProperty('required')) {
-                target.classList.add(inputErrorClass);
-            }
-        };
-        function numberCheck(target) {
-
-        }
-        function dataValidCheck(target, inputErrorClass) {
-            // if (target.dataset.hasOwnProperty('validator')) {
-            //     console.log(target.dataset.validator);
-            //     console.log(target.value);
-            //     numberCheck(target);
-            // }
-        }
         function formCheck(id) {
             var allFormInputes = document.querySelectorAll('#'+id+' input');
             allFormInputes.forEach(function (element) {
@@ -39,7 +52,6 @@
         form.addEventListener('blur', function(event) {
             if (event.target.tagName == "INPUT") {
                 dataRequiredCheck(event.target.getAttribute('id'), inputErrorClass);
-                dataValidCheck(event.target, inputErrorClass);
             }
         }, true);
         form.addEventListener('focus', function(event) {
@@ -48,8 +60,11 @@
             }
         }, true);
         form.addEventListener('submit', function(event) {
+            var checkList = form.querySelectorAll('input');
+            for (var i = 0, len = checkList.length; i < len; i++) {
+                dataRequiredCheck(checkList[i].id, inputErrorClass);
+            }
             event.preventDefault();
-            formCheck(formId);
         });
 
     };
