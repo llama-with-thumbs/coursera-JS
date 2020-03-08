f587cd39ca4f3c3b8d90970f42528230d5f0449d
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-03-07 21:27:43 -0500

    it cemmes to be done

--- 2.5/script.js
+++ 2.5/script.js
@@ -2,18 +2,22 @@
 
 // Код валидации формы
 (function() {
+    
     function numberCheck(target) {
         var min = target.dataset.hasOwnProperty('validatorMin') ? target.dataset.validatorMin : Number.NEGATIVE_INFINITY;
         var max = target.dataset.hasOwnProperty('validatorMax') ? target.dataset.validatorMax : Number.POSITIVE_INFINITY;
         return isFinite(target.value) && Number(target.value) <= max && Number(target.value) >= min;
     }
+
     function lettersCheck(target) {
         return (/^[a-zA-Zа-яА-Я]+$/.test(target.value));
     }
+
     function regexpCheck(target) {
         var regExp = new RegExp(target.dataset.validatorPattern);
         return regExp.test(target.value);
     }
+
     function dataValidCheck(target) {
         if (target.dataset.hasOwnProperty('validator')) {
             if (target.dataset.validator == 'number') {
@@ -27,43 +31,39 @@
         }
     }
 
-    function dataRequiredCheck(targetId, inputErrorClass) {
-        var target = document.getElementById(targetId);
-        if ((target.dataset.hasOwnProperty('required') && !target.value)||(target.value && !dataValidCheck(target))) {
-            target.classList.add(inputErrorClass);
+    function switchClasses(object, status, invalidClass, validClass) {
+        if (status) {
+            object.classList.contains(invalidClass) ? object.classList.remove(invalidClass) & object.classList.add(validClass) : object.classList.add(validClass); 
+        } else {
+            object.classList.contains(validClass) ? object.classList.remove(validClass) & object.classList.add(invalidClass) : object.classList.add(invalidClass); 
+        }
+    }
+
+    function dataRequiredCheck(target) {
+        if ((target.dataset.hasOwnProperty('required') && !target.value) || (target.value && !dataValidCheck(target))) {
+            return true;
         }
     }
 
     window.validateForm = function(formInput) {
-        var formId = formInput.formId;
-        var formValidClass = formInput.formValidClass;
-        var formInvalidClass = formInput.formInvalidClass;
-        var inputErrorClass = formInput.inputErrorClass;
         var form = document.getElementById('profile');
 
         form.addEventListener('blur', function(event) {
-            if (event.target.tagName == "INPUT") {
-                dataRequiredCheck(event.target.getAttribute('id'), inputErrorClass);
+            if (event.target.tagName == "INPUT" && dataRequiredCheck(event.target)) {
+                event.target.classList.add(formInput.inputErrorClass);
             }
         }, true);
+
         form.addEventListener('focus', function(event) {
             if (event.target.tagName == "INPUT") {
-                event.target.classList.remove('input_error');
+                event.target.classList.remove(formInput.inputErrorClass);
             }
         }, true);
+
         form.addEventListener('submit', function(event) {
             var checkList = Array.from(form.querySelectorAll('input'));
-            checkList.forEach(element => {
-                dataRequiredCheck(element.id, inputErrorClass);
-            });
-            
-            if (checkList.some(function(element) {
-                    return element.classList.contains(inputErrorClass);
-                })) {
-                form.classList.contains(formValidClass) ? form.classList.remove(formValidClass)&&form.classList.add(formInvalidClass) : form.classList.add(formInvalidClass);
-            } else {
-                form.classList.contains(formInvalidClass) ? form.classList.remove(formInvalidClass)&&form.classList.add(formValidClass) : form.classList.add(formInvalidClass);
-            }
+            var status = checkList.every(element => !dataRequiredCheck(element));
+            switchClasses(form, status, formInput.formInvalidClass, formInput.formValidClass);
             event.preventDefault();
         });
 
