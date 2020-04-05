b57954302bc19663d69bf30ccf5e4055687b9fd6
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-04-04 22:35:30 -0400

    half solved

--- finale/script.js
+++ finale/script.js
@@ -1,66 +1,146 @@
 'use strict';
+class EmojiCard {
+    open = false;
+    active = true;
+    domElement;
+    emoji;
+    domCardFace;
+    cardId;
 
-(function() {
-    function shuffleArray(array) {
-        for (var i = array.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-        return array;
+    constructor(domElement) {
+        this.domElement = domElement;
+        this.cardId = domElement.id;
+        // console.log(this.cardId);
     }
+}
+class GameBord {
+    cardList = {};
+    openCards = {};
 
-    function rippleFunc(event) {
-        var ripple = document.createElement("span");
-        document.getElementsByTagName("BODY")[0].appendChild(ripple);
-        ripple.classList.add("ripple");
+    greenCards = {};
+    redCards = {};
 
-        console.log(event.clientY, event.clientX);
-        ripple.style.top = (event.clientY - 30) + "px";
-        ripple.style.left = (event.clientX - 30) + "px";
-
-        ripple.classList.add("active");
-        setTimeout(function() {
-            ripple.classList.remove("active");
-        }, 400);
+    constructor (gameBordId, emojiConClass, quantity, cardDeck) {
+        this.multiplyCards(gameBordId, quantity);
+        this.placeEmoji(cardDeck, emojiConClass);
     }
-    window.rippleFlip = function(elementId) {
-        var frame = document.getElementById(elementId);
+    makeGreen (card1, card2, openCards) {
+        card1.domCardFace.style.backgroundColor = "green";
+        card1.domCardFace.style.borderColor = "green";
+        card1.active = false;
 
-        frame.addEventListener('click', function(event) {
-            console.log(event.target.classList);
-            if (event.target.classList.contains("emoji-card__face") || event.target.classList.contains("emoji-card__back")) {
-                rippleFunc(event);
-                var target = document.getElementById(event.target.id);
-                var face = document.getElementById(event.target.id).parentElement;
-                face.classList.contains("flipped") ? face.classList.remove("flipped") : face.classList.add("flipped");
+        card2.domCardFace.style.backgroundColor = "green";
+        card2.domCardFace.style.borderColor = "green";
+        card2.active = false;
+
+        delete openCards[card1.emoji];
+        console.log(openCards);
+    }
+    makeRed (openCards) {
+        Object.keys(openCards).forEach(function(emoji) {
+            if(openCards[emoji].active) {
+                openCards[emoji].domCardFace.style.backgroundColor = "red";
+                openCards[emoji].domCardFace.style.borderColor = "red";
+                openCards[emoji].active = true;
             }
         })
     }
+    makeWhite (card) {
+        card.domCardFace.style.backgroundColor = "white";
+        card.domCardFace.style.borderColor = "white";
+        card.active = true;
+    }
+    open (card) {
+        card.open = true;
+        card.domCardFace.parentElement.classList.add("flipped");
+        // this.addCard(card);
+        if (this.openCards.hasOwnProperty(card.emoji) && Object.keys(this.openCards).length == 1) {
+            this.makeGreen(card, this.openCards[card.emoji], this.openCards);
+            
+        } else if (Object.keys(this.openCards).length == 0) {
+            this.openCards[card.emoji] = card;
+        } else if (Object.keys(this.openCards).length == 1) {
+            this.openCards[card.emoji] = card;
+            this.makeRed(this.openCards);
+        } else {
+            for (var i = 0, emojiList = Object.keys(this.openCards); i < emojiList.length; i++) {
+                console.log(emojiList[i], i);
+                this.close(this.openCards[emojiList[i]]);
+                // console.log("this case");   
+                // this.openCards[card.emoji] = card;
+            }
+        }
 
-    window.multiplyCards = function (elementId, containerId, quantity) {
+        console.log(this.openCards);
+
+    }
+    close (card) {
+        card.domCardFace.parentElement.classList.remove("flipped");
+        card.open = false;
+        delete this.openCards[card.emoji];
+        this.makeWhite(card);
+        console.log(this.openCards);
+    }
+
+    flip (card) {
+        if (card.open && card.active) {
+            this.close(card);
+        } else if (!card.open && card.active) {
+            this.open(card);
+        } else if (card.open && !card.active) {
+            console.log("card green");
+        } else {
+            console.log("came card");
+        }
+    }
+
+    
+    multiplyCards (gameBordId, quantity) {
         for (var i = 0; i < quantity; i++) {
-            var card = document.getElementById(elementId).cloneNode(true);
+            var card = document.getElementById(gameBordId).firstElementChild.cloneNode(true);
             card.id = card.id + "-" + i;
+            this.cardList[card.id] = new EmojiCard(card);
             for (var j = 0, nodeList = card.querySelectorAll("*"); j < nodeList.length; j++) {
                 nodeList[j].id = nodeList[j].id + "-" + i;
+                nodeList[j].dataset.card = card.id;
             }
-            document.getElementById(containerId).appendChild(card);
+            document.getElementById(gameBordId).appendChild(card);
         }
-        return undefined;
+        document.getElementById(gameBordId).firstElementChild.remove();
     }
+    placeEmoji (cardDeck, emojiConClass) {
+        var selectedEmoji = shuffleArray(cardDeck).slice(0, Object.keys(this.cardList).length / 2);
+        selectedEmoji = selectedEmoji.concat(selectedEmoji);
+        
+        shuffleArray(selectedEmoji);
+        
+        for (var i = 0; i < Object.keys(this.cardList).length; i++) {
+            var card = this.cardList[Object.keys(this.cardList)[i]];
+            card.emoji = selectedEmoji[i];
+            card.domCardFace = card.domElement.querySelector("." + emojiConClass);
+            card.domCardFace.appendChild(document.createTextNode(card.emoji));
 
-    window.populateCards = function (elementClass, emojiList) {
-        shuffleArray(emojiList);
-        var cardList = Array.from(document.querySelectorAll("."+elementClass));
-        var colibEmojiList = emojiList.slice(0,cardList.length/2).concat(emojiList.slice(0,cardList.length/2));
-        shuffleArray(colibEmojiList);
-
-        for (var i = 0; i < cardList.length; i++) {
-            var t = document.createTextNode(colibEmojiList[i]);
-            document.getElementById(cardList[i].id).appendChild(t);
+            console.log(card.emoji, i);
+            card.domElement.addEventListener("click", function(event) {
+                bord.flip(bord.cardList[event.target.dataset.card]);
+            });
         }
-        return undefined;
     }
-}());
+}
+
+
+var bord;
+function setGame(bordId, emojiConClass, cardCount, cardDeck) {
+    bord = new GameBord(bordId, emojiConClass, cardCount, cardDeck);
+}
+
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
