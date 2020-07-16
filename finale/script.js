eefad6026247688bfbbac4248dda5b9b4e76e41f
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-07-15 21:32:27 -0400

    almost there. there is only animation on Lose and Win left

--- finale/script.js
+++ finale/script.js
@@ -2,7 +2,6 @@
 class EmojiCard {
     open = false;
     active = true;
-    deactivate;
 
     domElement;
     emoji;
@@ -21,9 +20,15 @@ class GameBord {
     greenCards = {};
     redCards = {};
 
-    gameScore = "Wasted";
+    gameScore = "Lose";
+
+    allEmoojis = [];
+    emojiContainerClass = "";
 
     constructor (gameBordId, emojiConClass, quantity, cardDeck) {
+        this.emojiContainerClass = emojiConClass;
+        this.allEmoojis = cardDeck;
+
         this.multiplyCards(gameBordId, quantity);
         this.placeEmoji(cardDeck, emojiConClass);
         this.setTimer("body");
@@ -31,7 +36,7 @@ class GameBord {
         // this.setShield("body", this.setTimer); // creates transparent layer behind the board ; and setting the timer
     
     }
-    
+
     makeGreen (openCards, greenCards) {
         // console.log("running makeGreen");
         // console.log(openCards);
@@ -40,6 +45,9 @@ class GameBord {
                 openCards[cardId].domCardFace.style.backgroundColor = "#5AD66F";
                 openCards[cardId].domCardFace.style.borderColor = "#5AD66F";
                 openCards[cardId].active = false;
+
+                openCards[cardId].domElement.classList.add("green"); // adding class for to the card wich signales that it is opened
+
 
                 greenCards[cardId] = openCards[cardId];
                 delete openCards[cardId];
@@ -75,6 +83,7 @@ class GameBord {
         var redCardIdList = Object.keys(this.redCards);
         
         // console.log("cardIdList.length", cardIdList.length);
+        
 
         if (cardIdList.length == 2 && redCardIdList.length == 0) {
             if(this.openCards[cardIdList[0]].emoji != this.openCards[cardIdList[1]].emoji) {
@@ -85,6 +94,7 @@ class GameBord {
                 // console.log("starting make green");
                 // console.log(this.openCards[cardIdList[0]].emoji, this.openCards[cardIdList[1]].emoji);
                 this.makeGreen(this.openCards, this.greenCards);
+                this.testResult(); //testing ether or not all cards are opened 
             }
             
             // console.log("openCards", this.openCards);
@@ -97,6 +107,7 @@ class GameBord {
             card.open = true;
             card.domCardFace.parentElement.classList.add("flipped");
             this.openCards[card.cardId] = card;
+            
 
         }
         // console.log("swiched after if");
@@ -105,7 +116,6 @@ class GameBord {
     close () {
         // console.log("runing close(card)");
         for (var i = 0, cardIdList = Object.keys(this.openCards); i < cardIdList.length; i++) {
-            // console.log("FOR for openCards");
 
             this.openCards[cardIdList[i]].domCardFace.parentElement.classList.remove("flipped");
             this.openCards[cardIdList[i]].open = false;
@@ -131,7 +141,7 @@ class GameBord {
     flip (card) {
         if (card.open && card.active) {
             // console.log("run close(card)");
-            this.close(card);
+            this.close();
         } else if (!card.open && card.active) {
             // console.log("run open(card)")
             this.open(card);
@@ -153,26 +163,47 @@ class GameBord {
     }
 
     placeEmoji (cardDeck, emojiConClass) {
-        var selectedEmoji = shuffleArray(cardDeck).slice(0, Object.keys(this.allCards).length / 2);
+        console.log("incide placeEmoji");
+        let selectedEmoji = shuffleArray(cardDeck).slice(0, Object.keys(this.allCards).length / 2);
         selectedEmoji = selectedEmoji.concat(selectedEmoji);
         
         shuffleArray(selectedEmoji);
+
+        console.log();
         
         for (var i = 0; i < Object.keys(this.allCards).length; i++) {
             var card = this.allCards[Object.keys(this.allCards)[i]];
+
             card.emoji = selectedEmoji[i];
+
             card.domCardFace = card.domElement.querySelector("." + emojiConClass);
+
+            // console.log(card.domCardFace.innerHTML); // current state of the card
+
+            if (card.domCardFace.innerHTML){
+                card.domCardFace.removeChild(card.domCardFace.firstChild);
+                card.domCardFace.appendChild(document.createTextNode(card.emoji));
+
+                card.domElement.lastElementChild.lastElementChild.removeChild(card.domElement.lastElementChild.lastElementChild.firstChild); // ***putting emoji on the card back
+                card.domElement.lastElementChild.lastElementChild.appendChild(document.createTextNode(card.emoji));
+
+                continue;
+            }
             card.domCardFace.appendChild(document.createTextNode(card.emoji));
 
-            // console.log(card.emoji, i);
+            card.domElement.lastElementChild.lastElementChild.appendChild(document.createTextNode(card.emoji));// ***putting emoji on the card back
+            card.domElement.lastElementChild.lastElementChild.style.fontSize = "40px";
+
             card.domElement.addEventListener("click", function(event) {
                 if (event.target.className != "emoji-card") {
+                    console.log("bord", bord);
                     bord.flip(bord.allCards[event.target.dataset.card]);
                     bord.setShield(bord.startTimer);
                 }                
             });
         }
     }
+    
 
     setShield(cb){
         function changeDepth(){
@@ -183,9 +214,11 @@ class GameBord {
             }
         }
         function reportWindowSize() {
-            console.log("resize");
+            console.log("resize");      
         }
-
+        if (document.getElementsByClassName("fullScreenBlock").length > 0) {
+            return undefined;
+        }
         let notice = document.createElement("DIV");
         let fullScreenBlock = document.createElement("DIV");
         let button = document.createElement("BUTTON");
@@ -194,9 +227,7 @@ class GameBord {
         fullScreenBlock.style.zIndex = -1;
         button.innerHTML = "Play again";
         button.onclick = function(){bord.restartGame(fullScreenBlock)};/*clicking the button is restarting the game*/
-        console.log("green cards", this.greenCards);
-        console.log("all cards", this.allCards);
-        console.log("opened cards", this.openCards);
+        
         result.innerHTML = this.gameScore;
 
         fullScreenBlock.appendChild(notice).classList.add("notice");
@@ -206,6 +237,8 @@ class GameBord {
 
         if(cb && typeof cb === "function") {
             cb(document.getElementsByClassName("timer")[0], changeDepth);
+        } else {
+            changeDepth();
         }
         window.onresize = reportWindowSize;
     }
@@ -213,7 +246,7 @@ class GameBord {
     setTimer(containerID) {
         if (!document.getElementsByClassName("timer").length) {
             let timer = document.createElement("H3");
-            let time = document.createTextNode("00:60");
+            let time = document.createTextNode("01:00");
             timer.appendChild(time);
             document.getElementById(containerID).appendChild(timer).classList.add("timer");
         }
@@ -233,13 +266,56 @@ class GameBord {
                 }
             }
         }
-        let myTimer = setInterval(function(){decreaseTime()}, 1000);
+        window.myTimer = setInterval(function(){decreaseTime()}, 200);
+    }
+    testResult() {
+        let allCardsQuantity = document.getElementsByClassName("emoji-card").length;
+        let flippedCardsQuantity = document.getElementsByClassName("flipped").length;
+        let greenCardsQuantity = document.getElementsByClassName("green").length;
+        
+        if (allCardsQuantity === greenCardsQuantity) {
+            this.gameScore = "Win";
+            document.getElementsByClassName("result")[0].innerHTML = this.gameScore;
+            clearInterval(myTimer);
+            if (document.getElementsByClassName("fullScreenBlock")[0].style.zIndex < 0) {
+                document.getElementsByClassName("fullScreenBlock")[0].style.zIndex = "1";
+            } else {
+                document.getElementsByClassName("fullScreenBlock")[0].style.zIndex = "-1";
+            }
+        }
+
     }
 
     restartGame (shield) {
         console.log("restart the game");
+
+        console.log("allCards: ", Object.keys(this.allCards).length);
+        console.log("green cards: ", Object.keys(this.greenCards).length);
+        console.log("open cards: ", Object.keys(this.openCards).length);
+        console.log("red cards: ", Object.keys(this.redCards).length);
+
+        for (let i = 0, cardIdList = Object.keys(this.allCards); i < cardIdList.length; i++) {
+            document.getElementById(cardIdList[i]).firstElementChild.classList.remove("flipped");
+            document.getElementById(cardIdList[i]).classList.remove("green");
+            this.greenCards = {};
+            this.redCards = {};
+            this.allCards[cardIdList[i]].open = false;
+            this.makeWhite(this.allCards[cardIdList[i]]);
+
+            // console.log(cardDeck, emojiConClass)
+            // this.placeEmoji(cardDeck, emojiConClass);
+
+            // delete this.openCards[cardIdList[i]];
+        }
+        this.placeEmoji(this.allEmoojis, this.emojiContainerClass);
+    
+        this.gameScore = "Lose";
+
+        console.log("openCards:", this.openCards);
+        this.close();
+
+        document.getElementsByClassName("timer")[0].innerHTML = "01:00";
         shield.remove();
-        
     }
 }
 
@@ -248,7 +324,6 @@ var bord;
 function setGame(bordId, emojiConClass, cardCount, cardDeck) {
 
     bord = new GameBord(bordId, emojiConClass, cardCount, cardDeck); /*starting the game here*/
-
     return undefined;
 }
 
